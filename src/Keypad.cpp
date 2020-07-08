e20e86e45c99953892459e3f12a4c8dae2242f7a
Mark Stanley <mstanley@technologist.com>
2020-07-08 19:10:09 -0400

    Fix waitForKey() bug https://github.com/Nullkraft/Keypad/issues/8

--- src/Keypad.cpp
+++ src/Keypad.cpp
@@ -212,8 +212,8 @@ char Keypad::waitForKey() {
 	char waitKey = NO_KEY;
 	while( (waitKey = getKey()) == NO_KEY ) {	// Block everything while waiting for a keypress.
 		delay(0);								// DO NOT REMOVE! Prevents problems with ESP8266 Arduino boards.
-		return waitKey;
 	}
+	return waitKey;
 }
 
 // Backwards compatibility function.
